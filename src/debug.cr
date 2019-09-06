a8d2cb6c3b356fc8b61328cd5cf129407392cb17
Sijawusz Pur Rahnama <sija@sija.pl>
2019-09-06 19:14:49 +0200

    Log each argument in a separate line

--- src/debug.cr
+++ src/debug.cr
@@ -37,63 +37,63 @@ module Debug
         %settings = ::Debug.settings
         %colors = %settings.colors
 
-        %str = String.build do |%str|
-          if %settings.show_path?
-            %relative_filepath = {{ file }}.lchop(Dir.current + "/")
-            if %relative_filepath
-              if %max_path_length = %settings.max_path_length
-                if %relative_filepath.size > %max_path_length
-                  %relative_filepath = "…" + %relative_filepath[-%max_path_length..]
+        {% for arg, i in args %}
+          %exp, %val =
+            %arg_expressions[{{ i }}], %arg_values[{{ i }}]
+
+          %str = String.build do |%str|
+            if %settings.show_path?
+              %relative_filepath = {{ file }}.lchop(Dir.current + "/")
+              if %relative_filepath
+                if %max_path_length = %settings.max_path_length
+                  if %relative_filepath.size > %max_path_length
+                    %relative_filepath = "…" + %relative_filepath[-%max_path_length..]
+                  end
                 end
+                %str << "#{%relative_filepath}:{{ line }}"
+                  .colorize(%colors[:path])
               end
-              %str << "#{%relative_filepath}:{{ line }}"
-                .colorize(%colors[:path])
-            end
 
-            %def_name = {{ @def && @def.name.stringify }}
-            if %def_name
-              %str << " (#{%def_name})"
-                .colorize(%colors[:method])
-            end
-            %str << " -- "
-          end
-
-          if %settings.show_backtrace?
-            %DEBUG_CALLER_PATTERN = /caller:Array\(String\)/i
-            %caller_list = caller
-
-            if %caller_list.any?(&.match(%DEBUG_CALLER_PATTERN))
-              while !%caller_list.empty? && %caller_list.first? !~ %DEBUG_CALLER_PATTERN
-                %caller_list.shift?
+              %def_name = {{ @def && @def.name.stringify }}
+              if %def_name
+                %str << " (#{%def_name})"
+                  .colorize(%colors[:method])
               end
-              %caller_list.shift?
-            end
-
-            {% if backtrace_offset > 0 %}
-              %caller_list.shift({{ backtrace_offset }})
-            {% end %}
-
-            if %caller = %caller_list.first?
-              %str << %caller
-                .colorize(%colors[:method])
               %str << " -- "
             end
-          end
 
-          {% for arg, i in args %}
-            %exp, %val =
-              %arg_expressions[{{ i }}], %arg_values[{{ i }}]
+            if %settings.show_backtrace?
+              %DEBUG_CALLER_PATTERN = /caller:Array\(String\)/i
+              %caller_list = caller
+
+              if %caller_list.any?(&.match(%DEBUG_CALLER_PATTERN))
+                while !%caller_list.empty? && %caller_list.first? !~ %DEBUG_CALLER_PATTERN
+                  %caller_list.shift?
+                end
+                %caller_list.shift?
+              end
+
+              {% if backtrace_offset > 0 %}
+                %caller_list.shift({{ backtrace_offset }})
+              {% end %}
+
+              if %caller = %caller_list.first?
+                %str << %caller
+                  .colorize(%colors[:method])
+                %str << " -- "
+              end
+            end
 
             %str << %exp
               .colorize(%colors[:expression])
             %str << " = "
               .colorize(%colors[:decorator])
             %val.to_debug(%str)
-            %str << " (" << typeof(%val).to_s.colorize(%colors[:type]) << ") "
-          {% end %}
-        end
+            %str << " (" << typeof(%val).to_s.colorize(%colors[:type]) << ')'
+          end
 
-        ::Debug.logger.log({{ severity }}, %str, {{ progname }})
+          ::Debug.logger.log({{ severity }}, %str, {{ progname }})
+        {% end %}
       end
 
       {% if args.size == 1 %}
