5c0a31ed36b77cb93a31e75f4a925c04fcf65ed9
Sijawusz Pur Rahnama <sija@sija.pl>
2019-09-07 03:13:05 +0200

    Enable Debug via compile-time check
    Without it makes it a no-op

--- src/debug.cr
+++ src/debug.cr
@@ -2,6 +2,10 @@ require "logger"
 require "colorize"
 
 module Debug
+  {% begin %}
+    ACTIVE = {{ env("DEBUG") == "1" }}
+  {% end %}
+
   class_property? enabled : Bool?
 
   def self.enabled? : Bool
@@ -27,74 +31,76 @@ module Debug
         {% end %}
       }
 
-      if ::Debug.enabled?
-        %arg_expressions = {
-          {% for arg in args %}
-            {{ arg.stringify }},
-          {% end %}
-        }
+      {% if ::Debug::ACTIVE %}
+        if ::Debug.enabled?
+          %arg_expressions = {
+            {% for arg in args %}
+              {{ arg.stringify }},
+            {% end %}
+          }
 
-        %settings = ::Debug.settings
-        %colors = %settings.colors
+          %settings = ::Debug.settings
+          %colors = %settings.colors
 
-        {% for arg, i in args %}
-          %exp, %val =
-            %arg_expressions[{{ i }}], %arg_values[{{ i }}]
+          {% for arg, i in args %}
+            %exp, %val =
+              %arg_expressions[{{ i }}], %arg_values[{{ i }}]
 
-          %str = String.build do |%str|
-            case %settings.location_detection
-            when .compile?
-              %relative_filepath = {{ file }}.lchop(Dir.current + "/")
-              if %relative_filepath
-                if %max_path_length = %settings.max_path_length
-                  if %relative_filepath.size > %max_path_length
-                    %relative_filepath = "…" + %relative_filepath[-%max_path_length..]
+            %str = String.build do |%str|
+              case %settings.location_detection
+              when .compile?
+                %relative_filepath = {{ file }}.lchop(Dir.current + "/")
+                if %relative_filepath
+                  if %max_path_length = %settings.max_path_length
+                    if %relative_filepath.size > %max_path_length
+                      %relative_filepath = "…" + %relative_filepath[-%max_path_length..]
+                    end
                   end
+                  %str << "#{%relative_filepath}:{{ line }}"
+                    .colorize(%colors[:path])
                 end
-                %str << "#{%relative_filepath}:{{ line }}"
-                  .colorize(%colors[:path])
-              end
 
-              %def_name = {{ @def && @def.name.stringify }}
-              if %def_name
-                %str << " (#{%def_name})"
-                  .colorize(%colors[:method])
-              end
-              %str << " -- "
+                %def_name = {{ @def && @def.name.stringify }}
+                if %def_name
+                  %str << " (#{%def_name})"
+                    .colorize(%colors[:method])
+                end
+                %str << " -- "
 
-            when .runtime?
-              %DEBUG_CALLER_PATTERN = /caller:Array\(String\)/i
-              %caller_list = caller
+              when .runtime?
+                %DEBUG_CALLER_PATTERN = /caller:Array\(String\)/i
+                %caller_list = caller
 
-              if %caller_list.any?(&.match(%DEBUG_CALLER_PATTERN))
-                while !%caller_list.empty? && %caller_list.first? !~ %DEBUG_CALLER_PATTERN
+                if %caller_list.any?(&.match(%DEBUG_CALLER_PATTERN))
+                  while !%caller_list.empty? && %caller_list.first? !~ %DEBUG_CALLER_PATTERN
+                    %caller_list.shift?
+                  end
                   %caller_list.shift?
                 end
-                %caller_list.shift?
+
+                {% if backtrace_offset > 0 %}
+                  %caller_list.shift({{ backtrace_offset }})
+                {% end %}
+
+                if %caller = %caller_list.first?
+                  %str << %caller
+                    .colorize(%colors[:method])
+                  %str << " -- "
+                end
               end
 
-              {% if backtrace_offset > 0 %}
-                %caller_list.shift({{ backtrace_offset }})
-              {% end %}
-
-              if %caller = %caller_list.first?
-                %str << %caller
-                  .colorize(%colors[:method])
-                %str << " -- "
-              end
+              %str << %exp
+                .colorize(%colors[:expression])
+              %str << " = "
+                .colorize(%colors[:decorator])
+              %val.to_debug(%str)
+              %str << " (" << typeof(%val).to_s.colorize(%colors[:type]) << ')'
             end
 
-            %str << %exp
-              .colorize(%colors[:expression])
-            %str << " = "
-              .colorize(%colors[:decorator])
-            %val.to_debug(%str)
-            %str << " (" << typeof(%val).to_s.colorize(%colors[:type]) << ')'
-          end
-
-          ::Debug.logger.log({{ severity }}, %str, {{ progname }})
-        {% end %}
-      end
+            ::Debug.logger.log({{ severity }}, %str, {{ progname }})
+          {% end %}
+        end
+      {% end %}
 
       {% if args.size == 1 %}
         %arg_values.first
