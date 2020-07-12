f8e8de1b24aa7f2b1082abbd9cb4290c9b6bfdf5
Sijawusz Pur Rahnama <sija@sija.pl>
2020-07-12 03:09:10 +0200

    Move the main macro logic inside the log block

--- src/debug.cr
+++ src/debug.cr
@@ -42,67 +42,67 @@ module Debug
           %colors = %settings.colors
 
           {% for arg, i in args %}
-            %exp, %val =
-              %arg_expressions[{{ i }}], %arg_values[{{ i }}]
+            ::Debug.logger.{{ severity.id }} do
+              %exp, %val =
+                %arg_expressions[{{ i }}], %arg_values[{{ i }}]
 
-            %str = String.build do |%str|
-              case %settings.location_detection
-              when .compile?
-                %relative_path = Path[{{ file }}].relative_to(Dir.current).to_s
-                if %relative_path
-                  if %max_path_length = %settings.max_path_length
-                    if %relative_path.size > %max_path_length
-                      %relative_path = "…" + %relative_path[-%max_path_length..]
+              String.build do |%str|
+                case %settings.location_detection
+                when .compile?
+                  %relative_path = Path[{{ file }}].relative_to(Dir.current).to_s
+                  if %relative_path
+                    if %max_path_length = %settings.max_path_length
+                      if %relative_path.size > %max_path_length
+                        %relative_path = "…" + %relative_path[-%max_path_length..]
+                      end
                     end
+                    %str << "#{%relative_path}:{{ line }}"
+                      .colorize(%colors[:path])
                   end
-                  %str << "#{%relative_path}:{{ line }}"
-                    .colorize(%colors[:path])
-                end
 
-                %def_name = {{ @def && @def.name.stringify }}
-                if %def_name
-                  %str << " (#{%def_name})"
-                    .colorize(%colors[:method])
-                end
-                %str << " -- "
+                  %def_name = {{ @def && @def.name.stringify }}
+                  if %def_name
+                    %str << " (#{%def_name})"
+                      .colorize(%colors[:method])
+                  end
+                  %str << " -- "
 
-              when .runtime?
-                %DEBUG_CALLER_PATTERN = /caller:Array\(String\)/i
-                %caller_list = caller
+                when .runtime?
+                  %DEBUG_CALLER_PATTERN = /caller:Array\(String\)/i
+                  %caller_list = caller
 
-                if %caller_list.any?(&.match(%DEBUG_CALLER_PATTERN))
-                  while !%caller_list.empty? && %caller_list.first? !~ %DEBUG_CALLER_PATTERN
+                  if %caller_list.any?(&.match(%DEBUG_CALLER_PATTERN))
+                    while !%caller_list.empty? && %caller_list.first? !~ %DEBUG_CALLER_PATTERN
+                      %caller_list.shift?
+                    end
                     %caller_list.shift?
                   end
-                  %caller_list.shift?
+
+                  {% if backtrace_offset > 0 %}
+                    %caller_list.shift({{ backtrace_offset }})
+                  {% end %}
+
+                  if %caller = %caller_list.first?
+                    %str << %caller
+                      .colorize(%colors[:method])
+                    %str << " -- "
+                  end
                 end
 
-                {% if backtrace_offset > 0 %}
-                  %caller_list.shift({{ backtrace_offset }})
-                {% end %}
+                %str << '\n' if %exp['\n']?
+                %str << %exp
+                  .colorize(%colors[:expression])
 
-                if %caller = %caller_list.first?
-                  %str << %caller
-                    .colorize(%colors[:method])
-                  %str << " -- "
+                %str << " = "
+                  .colorize(%colors[:decorator])
+
+                %val.to_debug.tap do |%pretty_val|
+                  %str << '\n' if %pretty_val['\n']?
+                  %str << %pretty_val
                 end
+                %str << " (" << typeof(%val).to_s.colorize(%colors[:type]) << ')'
               end
-
-              %str << '\n' if %exp['\n']?
-              %str << %exp
-                .colorize(%colors[:expression])
-
-              %str << " = "
-                .colorize(%colors[:decorator])
-
-              %val.to_debug.tap do |%pretty_val|
-                %str << '\n' if %pretty_val['\n']?
-                %str << %pretty_val
-              end
-              %str << " (" << typeof(%val).to_s.colorize(%colors[:type]) << ')'
             end
-
-            ::Debug.logger.{{ severity.id }} { %str }
           {% end %}
         end
       {% end %}
